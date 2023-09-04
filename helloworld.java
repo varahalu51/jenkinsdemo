36d0d9007d72eb3d7207069de0bf601bafaefd27
varahalu51 <nagam.svb@gmail.com>
2023-09-04 18:38:40 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class Helloworld{
 public static void main(String a[]){
-System.out.println("Welcome to first example for Jenkins ");
+System.out.println("Welcome to first example for Jenkins changed by srinu");
 }
 }
